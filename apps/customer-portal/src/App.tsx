6ea33ee65e6b01f054cad677a322d6f60f8c3ee8
Tushar <tusharbhatt0135@gmail.com>
2026-07-07 12:47:05 +0530

    Add @repo/utils package and integrate sum function in customer-portal

--- apps/customer-portal/src/App.tsx
+++ apps/customer-portal/src/App.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@repo/ui";
-
+import { sum } from "@repo/utils";
 function App() {
 	return (
 		<>
 			<h1>Complete life style</h1>
 			<Button title={"This is a button"} />
+			{sum(10, 20)}
 		</>
 	);
 }
